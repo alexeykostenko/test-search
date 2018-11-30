4cb3543e8b9090753be432161da79c4b29dd7377
Alexey Kostenko <aleksey.kostenko.dev@gmail.com>
2018-11-30 17:46:49 +0200

    fix route

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,33 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['namespace' => 'Api\v1', 'prefix' => 'v1'], function() {
+    /**
+     * @api {get} /search 1 Hotel search
+     * @apiGroup Hotel
+     *
+     * @apiParam {String} name Hotel Should also match on partial names
+     * @apiParam {Number} bedrooms Exact match
+     * @apiParam {Number} bathrooms Exact match
+     * @apiParam {Number} storeys Exact match
+     * @apiParam {Number} garages Exact match
+     * @apiParam {Number} minimum_price Minimum price
+     * @apiParam {Number} maximum_price Maximum price
+     *
+     * @apiParamExample {json} Request-Example:
+     *   {
+     *       "status": "success",
+     *       "data": [{
+     *           "name": "The Victoria",
+     *           "price": 374662,
+     *           "bedrooms": 4,
+     *           "bathrooms": 2,
+     *           "storeys": 1,
+     *           "garages": 2,
+     *           "garages": 2
+     *      }]
+     *   }
+     */
+     Route::get('search', ['as' => 'house.search', 'uses' => 'HouseController@search']);
+});
