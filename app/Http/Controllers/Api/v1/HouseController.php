fb994482ab45249b3c8208fe9227d7c5114e8d53
Alexey Kostenko <aleksey.kostenko.dev@gmail.com>
2018-11-30 17:27:38 +0200

    added House Controller

---
+++ app/Http/Controllers/Api/v1/HouseController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class HouseController extends Controller
+{
+    public function search()
+    {
+        return 'Houses';
+    }
+}
