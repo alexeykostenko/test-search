0ec0e1b04ec03411e5a3935e7c1b8e91ae951067
aleksey.kostenko.dev <aleksey.kostenko.dev@gmail.com>
2018-12-06 01:56:53 +0200

    added vuejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+window.VueResource = require('vue-resource');
 
 /**
  * The following block of code may be used to automatically register your
@@ -29,5 +30,40 @@ Vue.component('example-component', require('./components/ExampleComponent.vue'))
  */
 
 const app = new Vue({
-    el: '#app'
-});
+    el: '#app',
+    data: {
+        form: {
+            name: null,
+            price_min: null,
+            price_max: null,
+            bedrooms: null,
+            bathrooms: null,
+            storeys: null,
+            garages: null
+        },
+        houses:[]
+    },
+    methods: {
+        getHouses: function(){
+            var url = document.getElementById('search-houses-form').action;
+
+            this.$http.get(url, {
+                params: {
+                    name: this.form.name,
+                    bedrooms: this.form.bedrooms,
+                    bathrooms: this.form.bathrooms,
+                    storeys: this.form.storeys,
+                    garages: this.form.garages,
+                    price_min: this.form.price_min,
+                    price_max: this.form.price_max
+                },
+                responseType: 'json'
+            }).then(function(response){
+                console.log(response, response.houses);
+                if (response.body.status === 'success') {
+                    this.houses = response.body.houses;
+                }
+            });
+        }
+    }
+})
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Search Houses</title>
 
@@ -11,7 +12,7 @@
     </head>
   <body class="bg-light">
 
-    <div class="container">
+    <div id="app" class="container">
       <div class="py-5 text-center">
         <h2>Search Houses</h2>
         <p class="lead">You can easily find a house by filling out the form below.</p>
@@ -21,39 +22,25 @@
         <div class="col-md-4 order-md-2 mb-4">
           <h4 class="d-flex justify-content-between align-items-center mb-3">
             <span class="text-muted">House found</span>
-            <span class="badge badge-secondary badge-pill">3</span>
           </h4>
           <ul class="list-group mb-3">
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
+
+            <li v-for="house in houses" class="list-group-item d-flex justify-content-between lh-condensed">
               <div>
-                <h6 class="my-0">Product name</h6>
-                <small class="text-muted">Brief description</small>
+                <h6 class="my-0">@{{ house.name }}</h6>
               </div>
-              <span class="text-muted">$12</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Second product</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$8</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Third item</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$5</span>
+              <span class="text-muted">$@{{ house.price }}</span>
             </li>
           </ul>
         </div>
 
         <div class="col-md-8 order-md-1">
           <h4 class="mb-3">Search Form</h4>
-          <form class="needs-validation" novalidate>
+          <form id="search-houses-form" class="needs-validation" action="{{ route('house.search') }}">
+            @csrf
             <div class="mb-3">
               <label for="house-name">House name</label>
-              <input type="text" class="form-control" name="house_name" id="house-name" placeholder="The Victoria">
+              <input v-on:keyup="getHouses" v-model="form.name" type="text" class="form-control" name="house_name" id="house-name" placeholder="The Victoria">
             </div>
 
             <div class="row">
@@ -63,7 +50,7 @@
                     <div class="input-group-prepend">
                       <span class="input-group-text">$</span>
                     </div>
-                    <input type="number" class="form-control" name="price_min" id="minimum-price" placeholder="263604" required>
+                    <input v-on:keyup="getHouses" v-model="form.price_min" type="number" class="form-control" name="price_min" id="minimum-price" placeholder="263604" required>
                   </div>
               </div>
               <div class="col-md-6 mb-3">
@@ -72,7 +59,7 @@
                     <div class="input-group-prepend">
                       <span class="input-group-text">$</span>
                     </div>
-                    <input type="number" class="form-control" name="price_max" id="maximum-price" placeholder="263604" required>
+                    <input v-on:keyup="getHouses" v-model="form.price_max" type="number" class="form-control" name="price_max" id="maximum-price" placeholder="263604" required>
                   </div>
               </div>
             </div>
@@ -80,27 +67,27 @@
             <div class="row">
               <div class="col-md-3 mb-3">
                 <label for="bedrooms">Bedrooms</label>
-                <input type="number" class="form-control" name="bedrooms" id="bedrooms" placeholder="4">
+                <input v-on:keyup="getHouses" v-model="form.bedrooms" type="number" class="form-control" name="bedrooms" id="bedrooms" placeholder="4">
               </div>
 
               <div class="col-md-3 mb-3">
                 <label for="bathrooms">Bathrooms</label>
-                <input type="number" class="form-control" name="bathrooms" id="bathrooms" placeholder="3" required>
+                <input v-on:keyup="getHouses" v-model="form.bathrooms" type="number" class="form-control" name="bathrooms" id="bathrooms" placeholder="3" required>
               </div>
 
               <div class="col-md-3 mb-3">
                 <label for="storeys">Storeys</label>
-                <input type="number" class="form-control" name="storeys" id="storeys" placeholder="2" required>
+                <input v-on:keyup="getHouses" v-model="form.storeys" type="number" class="form-control" name="storeys" id="storeys" placeholder="2" required>
               </div>
 
               <div class="col-md-3 mb-3">
                 <label for="garages">Garages</label>
-                <input type="number" class="form-control" name="garages" id="garages" placeholder="1" required>
+                <input v-on:keyup="getHouses" v-model="form.garages" type="number" class="form-control" name="garages" id="garages" placeholder="1" required>
               </div>
             </div>
 
             <hr class="mb-4">
-            <button id="search-button" class="btn btn-primary btn-lg" type="button">Search</button>
+            <button id="search-button" class="btn btn-primary btn-lg" type="button" v-on:click="getHouses">Search</button>
           </form>
         </div>
       </div>
@@ -109,5 +96,6 @@
         <p class="mb-1">&copy; 2017-2018 Company Name</p>
       </footer>
     </div>
+    <script src="{{ mix('js/app.js') }}"></script>
   </body>
 </html>
